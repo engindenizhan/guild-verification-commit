e7e0b632c634845799bedf84619214ca9640e743
Mavi <45254356+engindenizhan@users.noreply.github.com>
2026-03-05 22:04:37 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:04:17.750Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T19:04:36.341Z
+Commit: Introduce real-time notifications
