71981005d5de3f7dd5a8e1c66c1da2196b4cc996
Mavi <45254356+engindenizhan@users.noreply.github.com>
2026-03-05 22:05:29 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:05:22.751Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T19:05:28.318Z
+Commit: Introduce real-time notifications
