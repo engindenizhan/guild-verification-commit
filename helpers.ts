be580875a44fdd4e2735f3ff994dcf59a1cd1beb
Mavi <45254356+engindenizhan@users.noreply.github.com>
2026-03-05 22:06:46 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:06:43.452Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:06:45.205Z
+Commit: Resolve null pointer exception in user service
