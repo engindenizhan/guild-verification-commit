947650f7a7060254b61f6870c7c604ffb1834472
Mavi <45254356+engindenizhan@users.noreply.github.com>
2026-03-05 22:04:19 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:47:54.893Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T19:04:17.750Z
+Commit: Patch security vulnerability in authentication
